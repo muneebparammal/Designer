e5fc189ea529113e46484518509093938494fcee
muneebparammal <your_email@example.com>
2026-04-07 13:57:01 +0400

    clarity updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,13 +31,14 @@ export default function RootLayout({
       <body className="font-[Manrope,sans-serif] antialiased">
         
         {/* Microsoft Clarity */}
-        <Script id="clarity" strategy="afterInteractive">
+         <Script id="ms-clarity-script" strategy="afterInteractive">
           {`(function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "rf62fqd02d");`}
         </Script>
+        
 
         {children}
       </body>
